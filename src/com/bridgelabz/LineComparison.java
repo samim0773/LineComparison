01d9d8b8b93f0acf65c0674d6768fa710f9af2c5
Samim Aktar <samimaktar0773@gmail.com>
2021-10-07 23:28:41 +0530

    Done UC-1 Calculate lenght

--- src/com/bridgelabz/LineComparison.java
+++ src/com/bridgelabz/LineComparison.java
@@ -1,8 +1,26 @@
 package com.bridgelabz;
+import java.util.Scanner;
 
 public class LineComparison {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Line Comparison Computation Program ");
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter the value of starting point according to x-axis  of frist line: ");
+		double x1 = sc.nextDouble();
+			
+		System.out.print("Enter the value of starting point according to y-axis  of frist line: ");
+		double y1 = sc.nextDouble();
+			
+		System.out.print("Enter the value of end point according to x-axis  of frist line: ");
+		double x2 = sc.nextDouble();
+			
+		System.out.print("Enter the value of end point according to x-axis  of frist line: ");
+		double y2 = sc.nextDouble();
+		
+		double length_of_line = Math.sqrt(Math.pow((x2 - x1),2) + Math.pow((y2 - y1 ),2));
+		System.out.println(length_of_line);
 		
 	}
 
